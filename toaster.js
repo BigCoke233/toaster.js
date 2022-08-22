5de1e8f69ec1c43afe2fbc796c31d2af678811d0
Eltrac Koalar <hi@guhub.cn>
2022-08-22 23:43:23 +0800

    :bulb: Add: 增加设置项

--- toaster.js
+++ toaster.js
@@ -7,36 +7,51 @@
  * jQuery required!
  */
 
-
 Toaster = function() {
+
     Toaster.amount = 0;
 
-    Toaster.send = function(m, color='rgb(197,197,106)') {
-        Toaster.dismiss('#toast-'+Toaster.amount);
+    Toaster.default = {
+        color: '#C5C56A',
+        autoClose: true,
+        autoCloseDelay: 2000
+    };
 
+    //core method
+    Toaster.toast = function(m, options = Toaster.default){
+        if($('#toast-'+Toaster.amount).length) Toaster.dismiss('#toast-'+Toaster.amount);
         Toaster.amount++;
 
         var id = 'toast-'+Toaster.amount;
         var selector = '#'+id;
-        $toast = $('<div class="toaster"></div>').attr('id',id).text(m).css({'background': color});
+        $toast = $('<div class="toaster"></div>').attr('id',id).text(m).css({'background': options.color || Toaster.default.color});
         $('body').append($toast);
         setTimeout(function(){
             $(selector).addClass('toasting');
         }, 1);
 
         $(selector).click(function(){
+            if(options.onClick) options.onClick();
             Toaster.dismiss(selector);
         });
 
-        setTimeout(function(){
-            Toaster.dismiss(selector);
-        }, 2000);
-    };
+        var autoClose=Toaster.default.autoClose;
+        if(options.autoClose!==undefined) autoClose=options.autoClose;
+        if(autoClose) {
+            setTimeout(function(){
+                Toaster.dismiss(selector);
+            }, options.autoCloseDelay || Toaster.default.autoCloseDelay);
+        }
+    }
+    Toaster.send = Toaster.toast; //for downward compatibility
 
-    Toaster.error = function(m) {
-        Toaster.send(m, 'rgb(244,66,54)');
+    //quick method
+    Toaster.error = function(m, options) {
+        Toaster.default.color='#F44236';
+        Toaster.toast(m, options);
     }
 
+    //dismiss given toast
     Toaster.dismiss = function(toast) {
         $(toast).removeClass('toasting').addClass('toast-dismissed');
         setTimeout(function(){
